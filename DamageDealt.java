f6c9c76fb92195f3ff5def9bad9f49acab953d8c
Rachel. M <70598758+s11715224@users.noreply.github.com>
2021-10-20 09:40:01 -0400

    v0.2
    AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA

--- DamageDealt.java
+++ DamageDealt.java
@@ -1,10 +1,12 @@
 // <robin/rachel macdonald>, AP CSA Unit 1 Challenge, Andrew Jackson High School, <10/20/2021>, <9:23>, v0.1
 
+import java.util.Scanner;
+
 public class DamageDealt   {
     public static void main(String[] args) {
-    
-    
-    
+        Scanner myScanner = new Scanner(System.in);
+        
+
     }
     
 }
